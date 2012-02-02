fbb1d8e63d9efc05e91fccae298a654594cb38af
dthourn@gmail.com <dthourn@gmail.com@254bd6ec-6c93-b1cd-8f65-821ec502a987>
2012-02-02 01:03:00 +0000

    changed doctype and content type back to html from xhtml

--- trunk/site/includes/header.php
+++ trunk/site/includes/header.php
@@ -1,11 +1,10 @@
-﻿<?xml version="1.0" ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
-        "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN"
+   "http://www.w3.org/TR/html4/strict.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml">
 
 <head>
-	<?php header('Content-Type: application/xhtml+xml; charset=utf-8'); ?>
+	<?php header('Content-Type: text/html; charset=utf-8'); ?>
 	<title><?php echo $title; ?></title>
 	<link href='css/main.css' rel='stylesheet' type='text/css' />
 </head>
